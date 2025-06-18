8e1ab5e544512bbd672bf529f840cf539b5a23b3
Parco <erikdd955@gmail.com>
2025-06-18 14:23:25 -0500

    implementar codigo a la clase Clientes  los atributos

--- src/main/java/elp/system/zapateria/model/Clientes.java
+++ src/main/java/elp/system/zapateria/model/Clientes.java
@@ -1,5 +1,23 @@
 package elp.system.zapateria.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.List;
+
+@Entity
+@Table(name = "clientes")
+@Data
 public class Clientes {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String apellidos;
+    private String direccion;
+    private Long celular;
+    private String correo;
+
+  
 }
